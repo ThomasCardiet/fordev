c44dd3148c099454cc41600aa1a9be07525600b8
Thomas <thom_car@yahoo.fr>
2022-03-09 21:58:51 +0100

    :construction: update Ajax and admin
    - update Ajax
    - add admin controller

--- migrations/Version20220306214959.php
+++ migrations/Version20220306214959.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220304210216 extends AbstractMigration
+final class Version20220306214959 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,8 @@ final class Version20220304210216 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE project_role (id INT AUTO_INCREMENT NOT NULL, project_id_id INT DEFAULT NULL, color VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, permissions LONGBLOB NOT NULL, INDEX IDX_6EF842726C1197C9 (project_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE project_role ADD CONSTRAINT FK_6EF842726C1197C9 FOREIGN KEY (project_id_id) REFERENCES project (id)');
+        $this->addSql('CREATE TABLE project_role (id INT AUTO_INCREMENT NOT NULL, project_id INT DEFAULT NULL, color VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, permissions LONGBLOB NOT NULL, INDEX IDX_6EF84272166D1F9C (project_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE project_role ADD CONSTRAINT FK_6EF84272166D1F9C FOREIGN KEY (project_id) REFERENCES project (id)');
     }
 
     public function down(Schema $schema): void
